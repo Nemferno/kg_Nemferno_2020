fd9d7a510d0283596c1a3f2dcd004e47e0a691a2
Nemferno <jasperbae@gmail.com>
2021-04-28 15:29:44 -0400

    Established Edge Cases
    The first edge case would be non-integer inputs such as 'ab'. The program will parse those inputs as 'NaN'. An exception would be thrown if one of them is NaN to tell the user to only input integer values.
    
    The second edge case would be a zero input. If we use the current algorithm, it will see that the input would be zero; thus ending the program. Hence, I implemented an edge case at the beginning of the algorithm to check if it’s zero.
    
    The third edge case would be a negative input. Integers are whole numbers that can be negative or positive. The algorithm assumes all inputs are positive integers. If a negative input is processed, it will not be able to map the phonetic value to the negative number. I implemented an edge case to handle any negative integer value.

--- main.js
+++ main.js
@@ -18,19 +18,40 @@ const PHONETIC_MAPPING = {
  * @returns a phonetic equivalent of the value
  */
 const toPhonetic = function (value) {
+	if (value === 0) {
+		return PHONETIC_MAPPING[value];
+	}
+
+	const isNegative = value < 0;
+	let cursor = Math.abs(value);
 	let phoneticStr = "";
-	while (value > 0) {
-		const digit = value % 10;
+	while (cursor > 0) {
+		const digit = cursor % 10;
 		const phonetic = PHONETIC_MAPPING[digit];
 		phoneticStr = phonetic + phoneticStr;
-		value = Math.floor(value / 10);
+		cursor = Math.floor(cursor / 10);
 	}
+
+	if (isNegative) {
+		phoneticStr = "Negative" + phoneticStr;
+	}
+
 	return phoneticStr;
 };
 
 // Get the arguments
 const args = process.argv.slice(2);
 
-// Map string to integers, integers to phonetic equivalents
-const output = args.map((arg) => parseInt(arg)).map(toPhonetic);
-console.log("Output:", output);
+try {
+	const integers = args.map((arg) => parseInt(arg));
+	// Check if there are invalid integers
+	if (integers.some(Number.isNaN)) {
+		throw new Error("Integers are NaN");
+	}
+
+	// Compute the output
+	const output = integers.map(toPhonetic);
+	console.log("Output:", output);
+} catch (error) {
+	console.error("Error: please input only integer values.");
+}
